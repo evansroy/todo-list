f655a0ba64b22744c26e3ca202eb184a09542b6c
Evans Roy <evansroysir@gmail.com>
2023-09-24 20:11:12 +0300

    Initial commit With Full back-end working and basic UI

---
+++ resources/js/vue/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="todoListContainer">
+       <div class="heading">
+         <h2 id="title">Todo List</h2>
+         <add-item-form/>
+       </div>
+       <list-item/>
+    </div>
+</template>
+
+<script>
+import AddItemForm from './AddItemForm.vue';
+import ListItem from './ListItem.vue';
+        export default {
+                components: {
+                    AddItemForm,
+                    ListItem
+                }
+        }
+</script>
+
+<style scoped>
+    .todoListContainer{
+        width: 350px;
+        margin: auto;
+    }
+    .heading{
+        background: #e6e6e6;
+        padding: 10px;
+    }
+    #title {
+        text-align: center;
+    }
+</style>
